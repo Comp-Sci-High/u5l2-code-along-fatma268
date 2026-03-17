8262602fa9b5075b647c8b8937dee67c66119fe8
fatma268 <fatma.diagne26@compscihigh.org>
2026-03-17 19:20:06 +0000

    commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 // import mongoose using require
 
+const mongoose = require("mongoose");
 
 // (in the terminal) npm i express mongoose
-
 
 const app = express();
 
@@ -16,23 +16,46 @@ app.use((req, res, next) => {
 
 // create a studentSchema with name, grade, advisory, and fav subject
 
+
+const studentSchema = new mongoose.Schema({
+  name: {type: String, required:true},
+  grade: {type: Number, required:true, default: 9},
+  advisory: {type: String, required:true},
+  favSubject: {type: String, default: "No Favorite Subject"},
+});
 // connect your schema to a model called Student
 
-// create a route hanlder for /g12 that returns every student in grade 12
+const Student = mongoose.model("Student", studentSchema, "Students");
+// create a route handler for /g12 that returns every student in grade 12
 
-// create a route hanlder for /me that returns yourself without using your name
+app.get("/g12", async (req,res) => {
+const students = await Student.find ({grade: 12})
+res.json(students)
+})
 
-// create a route hanlder for /friend that returns someone at your table using their name
+// create a route handler for /me that returns yourself without using your name
+
+ app.get("/me", async (req,res) => {
+const me = await Student.findOne({name: "Fatma Diagne"})
+res.json(me)
+})
+
+// create a route handler for /friend that returns someone at your table using their name
 
 async function startServer() {
   
   // In the SRV string, after the .net/ add myClass (e.g. ...mongodb.net/myClass?appName...)
   await mongoose.connect(
-    "mongodb+srv://SE12:CSH2026@cluster12.3ffmh.mongodb.net/mC?appName=Cluster12"
+    "mongodb+srv://SE12:CSH2026@cluster12.3ffmh.mongodb.net/myClass?appName=Cluster12"
   );
 
   // before you run your code, uncomment the following and add your info, then the info of a friend
-  //   await new ...({...}).save();
+    // await new Student({
+    //   name: "Fatma Diagne",
+    //   grade: 12,
+    //   advisory: "Benevolent Lotuses",
+    //   favSubject: "PSL"
+    // }).save();
 
   app.listen(3000, () => {
     console.log(`Server is running!`);
